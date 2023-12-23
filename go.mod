55bfbe5e49eac8b89c9f8136883d740eb8f51a67
mcrgnt <microgiant@yandex.ru>
2023-12-23 15:39:22 +0300

    v0.0.0 init

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mcrgnt/w
 
 go 1.21.4
+
+require github.com/go-gl/glfw v0.0.0-20231124074035-2de0cf0c80af
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package w
 
-func Test() {
-	println("JAJAJA!!")
+import (
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func createWindow(w, h int, title string) (window *glfw.Window, err error) {
+	if err = glfw.Init(); err != nil {
+		return
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 2)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err = glfw.CreateWindow(w, h, title, nil, nil)
+	if err != nil {
+		return
+	}
+
+	window.MakeContextCurrent()
+
+	return
 }
